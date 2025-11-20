3d8a03ac29908e41ad9c5e8e1fc237d9712c5cd9
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-20 19:31:18 +0200

    Implement MetaProperty, add make_property()
    Move __fields to SuperPerson, which necessitated adding the next:
            if "_fields" in clsdict:
                fields = clsdict["_fields"]
            else:
                for base in bases:
                    if hasattr(base, "_fields"):
                        fields = getattr(base, "_fields")
                        break
                else:
                    raise TypeError(f"{clsname} must define _fields or inherit it")

--- field.py
+++ field.py
@@ -20,36 +20,56 @@ class Property:
 
 
 class MetaProperty(type):
-    pass
+    def __new__(mcls, clsname, bases, clsdict):
+        if "_fields" in clsdict:
+            fields = clsdict["_fields"]
+        else:
+            for base in bases:
+                if hasattr(base, "_fields"):
+                    fields = getattr(base, "_fields")
+                    break
+            else:
+                raise TypeError(f"{clsname} must define _fields or inherit it")
+
+        for name in fields:
+
+            def make_property(name):
+                def getter(self):
+                    return getattr(self, name)
+
+                def setter(self, val):
+                    setattr(self, name, val)
+
+                def deleter(self):
+                    delattr(self, name)
+
+                return Property(getter, setter, deleter)
+
+            clsdict[name] = make_property(f"_{name}")
+
+        def __init__(self, *args, **kwargs):
+            for k, v in zip(fields, args):
+                self.__dict__[f"_{k}"] = v
+
+        clsdict["__init__"] = __init__
+
+        def __repr__(self):
+            key_val_pairs = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
+            return f"{self.__class__.__name__}({key_val_pairs})"
+
+        clsdict["__repr__"] = __repr__
+        return super().__new__(mcls, clsname, bases, clsdict)
 
 
 class SuperPerson(metaclass=MetaProperty):
     _fields = ["name", "age", "salary"]
 
 
-class Person:
-    def __init__(self, name):
-        self._name = name
-
-    def get_name(self):
-        return self._name
-
-    def set_name(self, val):
-        self._name = val
-
-    def del_name(self):
-        del self._name
-
-    name = Property(get_name, set_name, del_name)
-
-    def __repr__(self):
-        key_val_pairs = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
-        return f"{self.__class__.__name__}({key_val_pairs})"
+class Person(SuperPerson):
+    pass
 
 
 if __name__ == "__main__":
-    lara = Person("Elara Vance")
-    print(lara.name)
-    lara.name = "Elara Kael"
-    print(lara.name)
-    print(lara)
+    lara = Person("Elara Vance", 34, 68500.00)
+    max = Person("Maximus Kael", 28, 45200)
+    print(lara, max)
