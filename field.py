33218c5f5755da4054197eb05a0c972a88d94e50
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-20 17:41:37 +0200

    Start replacing Field with Property

--- field.py
+++ field.py
@@ -3,31 +3,44 @@
 # PYTHON_ARGCOMPLETE_OK
 
 
-class Field:
-    def __set_name__(self, owner, name):
-        self.name = f"_{owner.__name__}_{name}"
+class Property:
+    def __init__(self, fget, fset, fdel):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
 
-    def __init__(self, name=None):
-        pass
-
-    def __get__(self, instance, owner=None):
-        if not instance:
-            return self
-        return getattr(instance, self.name)
+    def __get__(self, instance, owner):
+        return self.fget(instance)
 
     def __set__(self, instance, value):
-        setattr(instance, self.name, value)
+        return self.fset(instance, value)
+
+    def __delete__(self, instance):
+        self.fdel(instance)
+
+
+class MetaProperty(type):
+    pass
+
+
+class SuperPerson(metaclass=MetaProperty):
+    _fields = ["name", "age", "salary"]
 
 
 class Person:
-    name = Field()
-    age = Field()
-    salary = Field()
+    def __init__(self, name):
+        self._name = name
 
-    def __init__(self, name, age, salary):
-        self.name = name
-        self.age = age
-        self.salary = salary
+    def get_name(self):
+        return self._name
+
+    def set_name(self, val):
+        self._name = val
+
+    def del_name(self):
+        del self._name
+
+    name = Property(get_name, set_name, del_name)
 
     def __repr__(self):
         key_val_pairs = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
@@ -35,6 +48,8 @@ class Person:
 
 
 if __name__ == "__main__":
-    lara = Person("Elara Vance", 34, 68500.00)
-    max = Person("Maximus Kael", 28, 45200)
-    print(lara, max)
+    lara = Person("Elara Vance")
+    print(lara.name)
+    lara.name = "Elara Kael"
+    print(lara.name)
+    print(lara)
